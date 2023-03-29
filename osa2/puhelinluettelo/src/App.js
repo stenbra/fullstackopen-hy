f98854f8b6c69ce11860d130072779c578534dd2
stenbra <stenbra7@gmail.com>
2023-03-29 15:37:59 +0300

    teht 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Filter =({onChangeThingy})=>{
@@ -28,12 +29,18 @@ const Numbers =({numbers})=>{
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response => { 
+      console.log('promise fulfilled') 
+      setPersons(response.data
+        )}
+      )}, 
+      [])
+    console.log('render', persons.length, 'persons')
+
   const [filteredList,setFiterList ]= useState([])  
   const [newName, setNewName] = useState('')
   const [showAll, setShowAll] = useState(true)
