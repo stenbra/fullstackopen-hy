9e56a200b0eb3db6d67e1fd0790250115d6a86ea
stenbra <stenbra7@gmail.com>
2023-04-01 16:40:21 +0300

    tehtävä 2.16

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
 
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 const Filter =({onChangeThingy})=>{
   return(<div>
     <div>
@@ -34,6 +44,8 @@ const Numbers =({numbers,remove})=>{
   </div>)
 }
 
+
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
 
@@ -52,6 +64,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [newNumber, setNewNumber] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const Add = (event) => { 
        event.preventDefault()  
@@ -69,6 +82,8 @@ const App = () => {
        const phoneObj = { name: newName, number:newNumber, id:persons.length+1} 
 
       personService.create(phoneObj).then(response => {
+       setErrorMessage(`Added ${newName}`)
+       setTimeout(()=> {setErrorMessage(null)},5000)
        setPersons(persons.concat(phoneObj))
        setNewName("")
        setNewNumber("")
@@ -107,7 +122,9 @@ const App = () => {
   }
   return (
     <div>
+      
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
         <Filter onChangeThingy={handleFilterNameChange}/>
       <h3>add a new</h3>
        <PersonFrom name={newName} number={newNumber} nameEvent= {handleNameChange} numberEvent={handleNumberChange} addFunction={Add}/>
