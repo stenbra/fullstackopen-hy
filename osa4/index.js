76e21a5b8432118deb27b94ec3f62f4536c16cf2
stenbra <stenbra7@gmail.com>
2023-04-13 11:13:44 +0300

    korjattu4.1

--- osa4/index.js
+++ osa4/index.js
@@ -1,19 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.rpuuql6.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
