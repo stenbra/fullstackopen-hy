f172e647f0180c8044fa1941462f0afa08bd4462
stenbra <stenbra7@gmail.com>
2023-04-17 12:08:13 +0300

    t 4.8

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,12 +1,13 @@
 const blogsRouter = require('express').Router()
+const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async(request, response) => {
+  const blog = await Blog.find({})
+    // .then(blogs => {
+    //   response.json(blogs)
+    // })
+  response.json(blog)
 })
   
 blogsRouter.post('/', (request, response) => {
